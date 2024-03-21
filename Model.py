d832aa3a5ae6da8818f331f95da737b231e63724
郝一鸣 <56924259+hacker-hym@users.noreply.github.com>
2024-03-21 03:16:50 +0000

    First submission, this program is only for demonstrating the PID controller

--- Model.py
+++ Model.py
@@ -1,15 +1,16 @@
 import math
 import random
-# v_left：左轮速度
-# v_right：右轮速度
-# wheel_track：轮距
-# x：x轴位置
-# y：y轴位置
-# theta：朝向角度
-# v：线速度
-# omega：角速度
-# Ts：采样时间
-# wheel_radius:轮子半径
+# v_left: velocity of the left wheel
+# v_right: velocity of the right wheel
+# wheel_track: distance between wheels
+# x: position along the x-axis
+# y: position along the y-axis
+# theta: orientation angle
+# v: linear velocity
+# omega: angular velocity
+# Ts: sampling time
+# wheel_radius: radius of the wheel
+
 
 class DifferentialDriveRobot:
     def __init__(self, v_left, v_right, wheel_track, x0, y0, theta0, Ts, wheel_radius):
--- TrajectoryPoint.py
+++ TrajectoryPoint.py
@@ -1,6 +1,6 @@
 class TrajectoryPoint:
     def __init__(self, x, y):
-        self.x = x  # x坐标
-        self.y = y  # y坐标
+        self.x = x  # x coordinate
+        self.y = y  # y coordinate
 
 
